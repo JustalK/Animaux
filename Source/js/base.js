0861259a1b822c0b8116d3668e71cdc70bc164c4
Latsuj <justal.kevin@gmail.com>
2015-10-13 18:16:12 +0200

    [8] Refactoring and adding the final design to the the menu

--- Source/js/base.js
+++ Source/js/base.js
@@ -13,6 +13,30 @@ $(document).ready(function() {
 		$(this).css('box-shadow', '0px 0px 0px 2px transparent');
 	});
 	
+	$("#niveau1").mouseenter(function(){
+		$(this).attr("src","img/menudoghover.png");
+	});
+	
+	$("#niveau1").mouseleave(function(){
+		$(this).attr("src","img/menudog.png");
+	});
+	
+	$("#niveau2").mouseenter(function(){
+		$(this).attr("src","img/menulionhover.png");
+	});
+	
+	$("#niveau2").mouseleave(function(){
+		$(this).attr("src","img/menulion.png");
+	});
+	
+	$("#niveau3").mouseenter(function(){
+		$(this).attr("src","img/menufishhover.png");
+	});
+	
+	$("#niveau3").mouseleave(function(){
+		$(this).attr("src","img/menufish.png");
+	});
+	
 	//Redirection directly in the link
 	$("#Nv1").click(function() {
 		$(this).css("cursor", 'url("../img/cursor.png"),url("../img/cursor2.png"),auto');
